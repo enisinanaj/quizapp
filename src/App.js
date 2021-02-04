7d32c13769b4c801ef3437f03f90dd1c2f752f10
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2021-02-05 00:08:27 +0100

    newProject

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../src/App.css";
+import 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import landingPage from './component/Landing/landingPage';
+import navigation from './component/Landing/Navigation';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css'; 
+import QuizCategories from './component/Landing/QuizCategories';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Switch>
+        <Route path="/"  component={landingPage} />
+        <Route path="/QuizCategories"  component={QuizCategories} />
+        
+       
+
+      </Switch>
+
+    </div>
+  </Router>
+  );
+}
+
+export default App;
