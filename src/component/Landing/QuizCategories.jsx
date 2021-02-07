7fcea27d39b951022ae2aa598a6abb7fac90ce3d
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2021-02-07 23:22:16 +0100

    first-structure of quizPage

--- src/component/Landing/QuizCategories.jsx
+++ src/component/Landing/QuizCategories.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {
-  MDBMask,  
-   MDBBtn ,
-   MDBView,
-   MDBContainer,} from "mdbreact";
+  MDBMask,
+  MDBBtn,
+  MDBView,
+  MDBContainer
+} from "mdbreact";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import { Fragment } from "react";
@@ -22,55 +23,52 @@ export default function quiz() {
 
   return (
     <div id="classicformpage">
-        < Navigation />
-        <MDBView>
-          <MDBMask className="d-flex justify-content-center align-items-center gradient">
-            <MDBContainer>
-      <div class= "categories">
-        <Fragment>
-        <Link to="/QuizPage" ><MDBBtn color="elegant">Books</MDBBtn></Link>
-        <MDBBtn color="unique">History</MDBBtn>
-        <MDBBtn color="pink">Celebrities</MDBBtn>
-        <MDBBtn color="purple">Movie</MDBBtn>
-        <MDBBtn color="deep-purple">Music</MDBBtn>
-        <MDBBtn color="indigo">Random</MDBBtn>
-        <MDBBtn color="light-blue">Sports</MDBBtn>
-        <MDBBtn color="cyan">General Knowledge</MDBBtn>
-        <MDBBtn color="dark-green">Games</MDBBtn>
-        {/*  <MDBBtn color="light-green">Light-green</MDBBtn>
-        <MDBBtn color="yellow">Yellow</MDBBtn>
-        <MDBBtn color="amber">Amber</MDBBtn>
-        <MDBBtn color="deep-orange">Deep-orange</MDBBtn>
-        <MDBBtn color="brown">Brown</MDBBtn>
-        <MDBBtn color="blue-grey">Blue-grey</MDBBtn>
-        <MDBBtn color="mdb-color">MDB</MDBBtn> */}
-      </Fragment>
-      </div>
-    
-      <Card class="heroCard" >
-        <CardContent >
-          <h1  color="white"  gutterBottom>
-            How to Play the Game
-        </h1>
-        <hr className="hr-light" />
+      < Navigation />
+      <MDBView>
+        <MDBMask className="d-flex justify-content-center align-items-center gradient">
+          <MDBContainer>
+            <div class="categories">
+              <Fragment>
+                <Link to="/QuizPage" ><MDBBtn color="elegant">Books</MDBBtn></Link>
+                <MDBBtn color="unique">History</MDBBtn>
+                <MDBBtn color="pink">Celebrities</MDBBtn>
+                <MDBBtn color="purple">Movie</MDBBtn>
+                <MDBBtn color="deep-purple">Music</MDBBtn>
+                <MDBBtn color="indigo">Random</MDBBtn>
+                <MDBBtn color="light-blue">Sports</MDBBtn>
+                <MDBBtn color="cyan">General Knowledge</MDBBtn>
+                <MDBBtn color="dark-green">Games</MDBBtn>
+                {/*  <MDBBtn color="light-green">Light-green</MDBBtn>
+                <MDBBtn color="yellow">Yellow</MDBBtn>
+                <MDBBtn color="amber">Amber</MDBBtn>
+                <MDBBtn color="deep-orange">Deep-orange</MDBBtn>
+                <MDBBtn color="brown">Brown</MDBBtn>
+                <MDBBtn color="blue-grey">Blue-grey</MDBBtn>
+                <MDBBtn color="mdb-color">MDB</MDBBtn> */}
+              </Fragment>
+            </div>
 
-          <Typography  color="white">
-            Each quiz has 4 options
-        </Typography>
-          <Typography variant="body2" component="p">
-            Each game has 2 lifelines normally.
-            <li> Two 50-50 chances which removes 2 of the wrong options </li>
-            <li> 5 hints. by clicking once it will remove one wrong option. you </li>
+            <Card class="heroCard" >
+              <CardContent >
+                <h1 color="white" gutterBottom>
+                  How to Play the Game
+                </h1>
+                <hr className="hr-light" />
 
-          </Typography>
-        </CardContent>
-        <CardActions>
-          {/*          <Button size="small">Learn More</Button> */}
-        </CardActions>
-      </Card>
-      </MDBContainer>
-          </MDBMask>
-        </MDBView>
+                <Typography color="white">
+                  Each quiz has 4 options
+                </Typography>
+                <Typography variant="body2" component="p">
+                  Each game has 2 lifelines normally.
+                  <li> Two 50-50 chances which removes 2 of the wrong options </li>
+                  <li> 5 hints. by clicking once it will remove one wrong option. you </li>
+
+                </Typography>
+              </CardContent>
+            </Card>
+          </MDBContainer>
+        </MDBMask>
+      </MDBView>
     </div>
 
   );
--- src/component/Landing/QuizPage.jsx
+++ src/component/Landing/QuizPage.jsx
@@ -1,18 +1,35 @@
 
-import React , { Fragment} from 'react';
+import React, { Fragment } from 'react';
 import 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { Card, CardBody, NavItem, NavLink, Nav, TabContent, TabPane, CardTitle, CardText, CardGroup, CardSubtitle } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from 'axios';
+import {
+  MDBMask,
+  MDBBtn,
+  MDBView,
+  MDBContainer, 
+  MDBIcon,
+} from "mdbreact";
+import './landing.css';
+import Navigation from './Navigation';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import IconButton from '@material-ui/core/IconButton';
+import HourglassEmptyOutlinedIcon from '@material-ui/icons/HourglassEmptyOutlined';
+import HighlightOutlinedIcon from '@material-ui/icons/HighlightOutlined';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import CloseIcon from '@material-ui/icons/Close';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 
 class QuizPlay extends React.Component {
   constructor(props) {
     super(props);
-   /*  this.state = {
-      counter: 0
-    }; */
+    /*  this.state = {
+       counter: 0
+     }; */
   }
   increaseCount = () => {
     this.setState({
@@ -22,19 +39,55 @@ class QuizPlay extends React.Component {
 
   render() {
     return (
-      <div class= "categories">
-      <Fragment>
-      <div className="questions">
-            <h5> What is the capital of France?> </h5>
-            
-                     
-       
-   
+      <div id="classicformpage">
+        < Navigation />
+        <MDBView>
+          <MDBMask className="d-flex justify-content-right align-items-center gradient">
+            <MDBContainer>
+              <Fragment>
+                <div className="questions">
+                  <div className="lifeLine-container">
+                    <p>
+                      <span> <IconButton color="inherit">
+                       <FavoriteBorderOutlinedIcon /> </IconButton> 2 </span>
+                    </p>
+                    <p>
+                      <span> <IconButton color="inherit">
+                       <HighlightOutlinedIcon /> </IconButton> 2 hints </span>
+                    </p>
+                  </div>
+                  <div>
+                    <p>
+                      <span>1 of 15</span>
+                      <span> 
+                      <IconButton color="inherit">
+                       <HourglassEmptyOutlinedIcon />timer  00:10 </IconButton>  </span>
+                    </p>
+                  </div>
+                  <h5> What is the capital of France?> </h5>
+                  <div className="option-container">
+                    <p className="option">New York </p>
+                    <p className="option">London </p>
+                  </div>
+                  <div className="option-container">
+                    <p className="option">Paris </p>
+                    <p className="option">Dublin </p>
+ 
+                  </div>
+                  <div className="button-contaier">
+                   <MDBBtn outline color="white">Previous <ArrowBackIosIcon/> </MDBBtn>
+                   <MDBBtn color="white">Next<NavigateNextIcon/>  </MDBBtn>
+                   <MDBBtn color="red">Quit <CloseIcon/> </MDBBtn>
+                   
+                  </div>
+                </div>
+              </Fragment>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
       </div>
-    </Fragment>
-    </div>
-  
-  
+
+
     );
 
 
@@ -43,6 +96,6 @@ class QuizPlay extends React.Component {
 
 export default QuizPlay;
 
-   {/*  <p>Counter: {this.state.counter} </p>
+{/*  <p>Counter: {this.state.counter} </p>
         <button onClick={this.increaseCount}  >Click me</button>
       </div> */}
